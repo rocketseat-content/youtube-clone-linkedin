a74e80604f16ce1d269d4786def219cdb91e2106
Guilherme Rodz <guilherme.rodrigues.profissional@gmail.com>
2020-08-18 12:26:45 -0300

    Setup loading props

--- src/components/LeftColumn/index.tsx
+++ src/components/LeftColumn/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
+import LoadingProfilePanel from '../Shimmer/LoadingProfilePanel';
 import ProfilePanel from './ProfilePanel';
 import HashtagPanel from './HashtagPanel';
 
 import { Container } from './styles';
 
-const LeftColumn: React.FC = () => {
+const LeftColumn: React.FC<LoadingProps> = ({ isLoading }) => {
   return (
     <Container className="left-column">
-      <ProfilePanel />
-      <HashtagPanel />
+      {isLoading ? (
+        <LoadingProfilePanel />
+      ) : (
+        <>
+          <ProfilePanel />
+          <HashtagPanel />
+        </>
+      )}
     </Container>
   );
 };
---
+++ src/components/Shimmer/LoadingProfilePanel/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const LoadingProfilePanel: React.FC = () => {
+  return <div />;
+};
+
+export default LoadingProfilePanel;
