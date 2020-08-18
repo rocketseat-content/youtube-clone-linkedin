3445ccd15c267b5330f36cb3f3c696dedf5a49a2
Guilherme Rodz <guilherme.rodrigues.profissional@gmail.com>
2020-08-18 12:35:27 -0300

    Applied first shimmer effect

--- src/components/Shimmer/LoadingProfilePanel/index.tsx
+++ src/components/Shimmer/LoadingProfilePanel/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import Panel from '../../Panel';
+import Skeleton from '../../Skeleton';
+
+import { Container } from './styles';
 
 const LoadingProfilePanel: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Panel className="no-shadow">
+        <Skeleton className="bg-skeleton" />
+        <span>
+          <Skeleton className="avatar-skeleton" />
+          <Skeleton className="row-skeleton" />
+          <Skeleton className="row-skeleton" />
+        </span>
+      </Panel>
+    </Container>
+  );
 };
 
 export default LoadingProfilePanel;
